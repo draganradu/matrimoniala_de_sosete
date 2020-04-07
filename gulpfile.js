6d14d8d6baa58136c229938416454fa71ac7e35e
radu.dragan <radu.dragan@2checkout.com>
2020-04-07 12:16:05 +0300

    Converted from css to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const { src, dest, parallel, series } = require('gulp');
 const del = require('del');
 const archiver = require('gulp-archiver');
+const concat = require('gulp-concat');
+
+const minify = require('gulp-minify');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 
 function arhive() {
@@ -13,7 +18,15 @@ function deleteFile() {
     return del(['archive.zip']);
 }
 
+function style() {
+    return src('./style/*.scss')
+           .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+           .pipe(concat('style.css', {newLine: ''}))
+           .pipe(dest('./style/'));
+}
+
 exports.arhive = arhive;
 exports.deleteFile = deleteFile;
+exports.style = style;
 
-exports.default = series( deleteFile, arhive);
+exports.default = series( deleteFile, style, arhive);
